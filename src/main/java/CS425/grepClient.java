e48352b991bbae0164feffdb84499bd0ec4aed0b
shaowen2 <shaowen2@illinois.edu>
2017-09-10 23:33:18 -0500

    空的，啥都没写呢

---
+++ src/main/java/CS425/grepClient.java
@@ -0,0 +1,12 @@
+package CS425;
+
+/**
+ * Hello world!
+ *
+ */
+public class grepClient
+{
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ src/main/java/CS425/grepServer.java
@@ -0,0 +1,10 @@
+package CS425;
+
+
+public  class grepServer{
+
+
+    public static void main(String[] a){
+
+    }
+}
